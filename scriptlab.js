fe5ce5f39490eda7d208c7fa5ab58147f657c44c
Odicienta <82636064+odicienta@users.noreply.github.com>
2023-03-12 17:32:40 -0600

    Create scriptlab.js

---
+++ scriptlab.js
@@ -0,0 +1,32 @@
+let redBtn = document.getElementById("red");
+let lightblueBtn = document.getElementById("lightblue");
+let purpleBtn = document.getElementById("purple");
+let blueBtn = document.getElementById("blue");
+let darkblueBtn = document.getElementById("darkblue");
+let brownBtn = document.getElementById("brown");
+let imgchange = document.getElementById("imgchange");
+
+redBtn.onclick = function() {
+
+    imgchange.src = "./imageslab/60528001.jpg";
+}
+
+lightblueBtn.onclick = function() {
+    imgchange.src = "./imageslab/A_los_pits.webp";
+}
+
+purpleBtn.onclick = function() {
+    imgchange.src = "./imageslab/cars_ramone__BD54-456A-92B5-79382D5604C2xx900xx0.jpg";
+}
+
+blueBtn.onclick = function() {
+    imgchange.src = "./imageslab/descarga.jpeg";
+}
+
+darkblueBtn.onclick = function() {
+    imgchange.src = "./imageslab/erreytytytryrt-9aac.jpg";
+}
+
+brownBtn.onclick = function() {
+    imgchange.src = "./imageslab/Mate.webp";
+}
